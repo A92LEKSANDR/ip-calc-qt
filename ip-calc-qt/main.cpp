4e7aa2d4cc90ffa3bf7d44e40c2a02631f336a12
A92LEKSANDR <k92aco@gmail.com>
2024-08-11 14:40:09 +0300

    upd

--- ip-calc-qt/main.cpp
+++ ip-calc-qt/main.cpp
@@ -1,11 +1,14 @@
-#include "mainwindow.h"
-
+// #include "mainwindow.h"
+#include "ipcalculator.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    // MainWindow w;
+    // w.show();
+    IPCalculator ipcalc;
+    ipcalc.show();
+
     return a.exec();
 }
